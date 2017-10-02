7e56a41bd81e03315b5392dcdaa2548a8cbe6dca
91077747 <91077747@mvusd.net>
2017-10-02 10:59:53 -0700

    HYIHIH

--- src/org/teachingkidsprogramming/section03ifs/HiLow2.java
+++ src/org/teachingkidsprogramming/section03ifs/HiLow2.java
@@ -8,13 +8,19 @@ public class HiLow2
 {
   public static void main(String[] args)
   {
-    int answer = (int) (Math.random() * 100 + 1);
+    int upperRange = MessageBox.askForNumericalInput("enter upper range ");
+    int answer = (int) (Math.random() * upperRange + 1);
     //System.out.println("The answer is" + answer);
     //int answer = 40;
-    int lives = 8;
-    for (int i = 1; i <= lives; i++)
+    int lives = MessageBox.askForNumericalInput("How many Guesses");
+    for (int i = 1; i < lives; i++)
     {
       int guess = MessageBox.askForNumericalInput("what is your guess?");
+      while (guess < 1 || guess > upperRange)
+      {
+        MessageBox.showMessage("please enter a valid guess between 1 and" + upperRange);
+        guess = MessageBox.askForNumericalInput("what is your guess");
+      }
       if (guess == answer)
       {
         Sound.playBeep();

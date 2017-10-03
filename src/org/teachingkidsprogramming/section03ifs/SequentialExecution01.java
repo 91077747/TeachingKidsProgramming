f2dbf10c79b376b21b29e9da86bc5b1c6c3ac74c
91077747 <91077747@mvusd.net>
2017-10-03 09:36:33 -0700

    started SequentialExecution01

---
+++ src/org/teachingkidsprogramming/section03ifs/SequentialExecution01.java
@@ -0,0 +1,22 @@
+package org.teachingkidsprogramming.section03ifs;
+
+public class SequentialExecution01
+{
+  //looking at Sequential execution, 
+  // the order operation run in program
+  public static void main(String[] arge)
+  {
+    System.out.println("In main #1");
+    myMethod1();
+    System.out.println("In main #2");
+  } // end of main method
+  public static void myMethod1()
+  {
+    System.out.println("In myMethod1 #1");
+    myMethod2();
+  }
+  public static void myMethod2()
+  {
+    System.out.println("In myMethod2 #1");
+  }
+}// end of class
